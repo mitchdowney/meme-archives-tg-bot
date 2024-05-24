6da96581e29d20d0dbe477f3a7268baed54d610a
Mitch Downey <mitchldowney@gmail.com>
2024-05-24 14:16:43 -0500

    Update checkTelegramSecretKey.ts

--- src/middleware/checkTelegramSecretKey.ts
+++ src/middleware/checkTelegramSecretKey.ts
@@ -1,17 +1,12 @@
-import { Unauthorized } from 'http-errors'
+import { Forbidden } from 'http-errors'
 import { config } from '../config'
-import { sendMessage } from '../services/telegram'
 
 export const checkBotAppSecretKey = (req, res, next) => {
   const secretTokenHeader = req.headers['x-telegram-bot-api-secret-token']
   const secretKey = config.BOT_APP_SECRET_TOKEN
 
   if (secretTokenHeader !== secretKey) {
-    const chat_id = req?.body?.message?.chat?.id
-    if (chat_id) {
-      sendMessage(chat_id, 'Permission denied.')
-    }
-    throw new Unauthorized('Invalid bot app secret key.')
+    throw new Forbidden('Permission denied. Invalid bot app secret key.')
   } else {
     next()
   }
