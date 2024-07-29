82422f0310c0947741f0e0bd25c1863e86818967
Mitch Downey <mitchldowney@gmail.com>
2024-07-28 20:55:10 -0500

    Add prevent_border_image to upload_image

--- src/services/galleryAPI.ts
+++ src/services/galleryAPI.ts
@@ -58,6 +58,7 @@ type GalleryUploadImage = {
     filename: string
     buffer: Buffer
   } | null
+  prevent_border_image?: boolean
 }
 
 const getContentTypeFromFilename = (filename: string) => {
@@ -82,7 +83,7 @@ const createImageFormData = (data: GalleryUploadImage, id?: number) => {
     tagTitles: JSON.stringify(data.tagTitles || []),
     artistNames: JSON.stringify(data.artistNames || []),
     slug: data.slug,
-    prevent_border_image: config.GALLERY_IMAGE_PREVENT_BORDER_IMAGE,
+    prevent_border_image: data.prevent_border_image || config.GALLERY_IMAGE_PREVENT_BORDER_IMAGE,
     preview_crop_position: config.GALLERY_IMAGE_PREVIEW_CROP_POSITION
   }
 
